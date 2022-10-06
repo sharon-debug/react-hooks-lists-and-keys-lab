299c5be9e73c03f58a3ad7b3433962bd4ad72de3
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-10-06 18:24:20 +0300

    complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,14 @@
-import React from "react";
+//import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
-
-export default NavBar;
+  //  const links = ["home", "about", "projects"];
+  
+    return <nav>
+  <a href="#home">home</a>
+  <a href="#about">about</a>
+  <a href="#projects">projects</a>
+    
+    </nav>;
+  }
+  
+  export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,22 @@
 import React from "react";
+//wimport user from "../data/user";
 
 function ProjectItem({ name, about, technologies }) {
+  console.log(technologies);
+
+  const tech=technologies.map((tech)=>{
+    return <span key={tech}>{tech}</span>})
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
+
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+
+       <span> {tech} </span>
+        
       </div>
     </div>
   );
